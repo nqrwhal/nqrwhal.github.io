2b64042f3e48f7b41f02d87db1c32aa3ee9369d3
nqrwhal <yum011@ucsd.edu>
2022-04-30 03:51:53 -0700

    master

--- MD.java
+++ MD.java
@@ -2,29 +2,33 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MD {
 
-    public static ArrayList<String> getLinks(String markdown) {
+    public static ArrayList<String> getLinks(List<String> markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then read link upto next )
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+        for(String x: markdown) {
+        	if(x.contains("[")) {
+        		if(x.contains("]")) {
+        			if(x.contains("(")) {
+        				if(x.contains(")")) {
+        					int openParen = x.indexOf("(");
+        					int closeParen = x.indexOf(")");
+        					toReturn.add(x.substring(openParen + 1, closeParen));
+        				}
+        			}continue;
+        		}continue;
+        	}continue;
         }
-
         return toReturn;
     }
 
 
+
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of("1.md");
-        String content = Files.readString(fileName);
+        List<String> content = Files.readAllLines(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
     }
