6b9bc8560ab384b006b301871d20eec9757b34d2
nqrwhal <yum011@ucsd.edu>
2022-04-30 03:57:10 -0700

    master

--- MD.java
+++ MD.java
@@ -2,34 +2,53 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MD {
 
-    public static ArrayList<String> getLinks(List<String> markdown) {
+    public static ArrayList<String> getLinks(List<String> md) {
         ArrayList<String> toReturn = new ArrayList<>();
-        for(String x: markdown) {
-        	if(x.contains("[")) {
-        		if(x.contains("]")) {
-        			if(x.contains("(")) {
-        				if(x.contains(")")) {
-        					int openParen = x.indexOf("(");
-        					int closeParen = x.indexOf(")");
-        					toReturn.add(x.substring(openParen + 1, closeParen));
-        				}
-        			}continue;
-        		}continue;
-        	}continue;
+        for (String x : md) {
+            ArrayList<String> sep = new ArrayList<String>();
+            boolean j = true;
+            while (j) {
+                if (x.contains("[") && x.contains("]") && x.contains("(") && x.contains(")") && x.length() > 0) {
+
+                    int a = x.indexOf("[");
+                    int b = x.indexOf("]");
+                    int c = x.indexOf("(");
+                    int d = x.indexOf(")");
+                    if (a < b && b < c && c < d) {
+                        String y = x.substring(a, d);
+                        sep.add(y);
+                        if (d < x.length()) {
+                            x = x.substring(d + 1);
+                        } else {
+                            break;
+                        }
+                    }
+
+                }else{
+                    j = false;
+                }
+            }
+            for (String e : sep) {
+                if (e.indexOf("[") + 1 != e.indexOf("]") && e.indexOf("]") + 1 == e.indexOf("(")) {
+                    int openParen = e.indexOf("(");
+                    
+                    toReturn.add(e.substring(openParen + 1, e.length()));
+
+                }
+            }
         }
+
         return toReturn;
     }
 
-
-
     public static void main(String[] args) throws IOException {
-        Path fileName = Path.of("1.md");
+        Path fileName = Path.of(args[0]);
         List<String> content = Files.readAllLines(fileName);
         ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        System.out.println(links);
     }
 }
